2250b0bfef5649ae7cc3a6333d8a032fcb0a3e86
Wenping Guo <ybyygu@gmail.com>
2022-11-21 18:43:00 +0800

    New test for OWL-QN algorithm ported from R/lbfgs package

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,27 +1,20 @@
-// rosenbrock
-// :PROPERTIES:
-// :header-args: :tangle tests/rosenbrock.rs
-// :END:
-// # The Rosenbrock function
-
-// [[file:~/Workspace/Programming/gosh-rs/lbfgs/lbfgs.note::*rosenbrock][rosenbrock:1]]
-//! Multidimensional Rosenbrock test function
-//!
-//! Defined as
-//!
-//! `f(x_1, x_2, ..., x_n) = \sum_{i=1}^{n-1} \left[ (a - x_i)^2 + b * (x_{i+1} - x_i^2)^2 \right]`
-//!
-//! where `x_i \in (-\infty, \infty)`. The parameters a and b usually are: `a = 1` and `b = 100`.
-//!
-//! The global minimum is at `f(x_1, x_2, ..., x_n) = f(1, 1, ..., 1) = 0`.
-//
-//! # Reference
-//! - https://en.wikipedia.org/wiki/Rosenbrock_function
-
 use approx::*;
 
+/// Multidimensional Rosenbrock test function
+///
+/// Defined as
+///
+/// `f(x_1, x_2, ..., x_n) = \sum_{i=1}^{n-1} \left[ (a - x_i)^2 + b * (x_{i+1} - x_i^2)^2 \right]`
+///
+/// where `x_i \in (-\infty, \infty)`. The parameters a and b usually are: `a = 1` and `b = 100`.
+///
+/// The global minimum is at `f(x_1, x_2, ..., x_n) = f(1, 1, ..., 1) = 0`.
+///
+/// # Reference
+///
+/// - https://en.wikipedia.org/wiki/Rosenbrock_function
 #[test]
-fn test_lbfgs() {
+fn test_lbfgs_rosenbrock() {
     use liblbfgs::{default_evaluate, default_progress, lbfgs, Progress};
 
     const N: usize = 100;
@@ -60,4 +53,31 @@ fn test_lbfgs() {
     assert_relative_eq!(0.2500, x[0], epsilon = 1e-4);
     assert_relative_eq!(0.0575, x[1], epsilon = 1e-4);
 }
-// rosenbrock:1 ends here
+
+#[test]
+fn test_lbfgs_booth() {
+    use liblbfgs::{default_progress, lbfgs, Progress};
+
+    const N: usize = 2;
+
+    // 1. Initialize the variables
+    let mut x = [-1.2, 1.0];
+
+    // 2. Defining how to evaluate function and gradient
+    let evaluate = |x: &[f64], gx: &mut [f64]| {
+        let x1 = x[0];
+        let x2 = x[1];
+        let mut fx = (x1 + 2.0 * x2 - 7.0).powi(2) + (2.0 * x1 + x2 - 5.0).powi(2);
+        gx[0] = 10.0 * x1 + 8.0 * x2 - 34.0;
+        gx[1] = 8.0 * x1 + 10.0 * x2 - 38.0;
+
+        Ok(fx)
+    };
+
+    let _ = lbfgs()
+        .minimize(&mut x, evaluate, default_progress())
+        .expect("lbfgs minimize");
+
+    assert_relative_eq!(x[0], 1.0, epsilon = 1E-6);
+    assert_relative_eq!(x[1], 3.0, epsilon = 1E-6);
+}
